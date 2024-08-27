5460d8d68c2aff0362994b3768bcefe1d2c81873
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-08-27 22:47:26 +0200

    Add countdown timer functionality

--- popup/popup.js
+++ popup/popup.js
@@ -2,6 +2,20 @@ const addTaskBtn = document.getElementById("add-task-btn");
 const startTimerBtn = document.getElementById("start-timer-btn");
 const resetTimerBtn = document.getElementById("reset-timer-btn");
 
+function updateTime() {
+  chrome.storage.local.get(["timer"], (res) => {
+    const time = document.getElementById("time");
+    const minutes = `${25 - Math.ceil(res.timer / 60)}`.padStart(2, "0");
+    let seconds = "00";
+    if (res.timer % 60 != 0) {
+      seconds = `${60 - (res.timer % 60)}`.padStart(2, "0");
+    }
+    time.textContent = `${minutes}:${seconds}`;
+  });
+}
+
+setInterval(updateTime, 1000);
+
 startTimerBtn.addEventListener("click", () => {
   chrome.storage.local.get(["isRunning"], (res) => {
     chrome.storage.local.set(
